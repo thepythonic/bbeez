2bf179634966dab891a4c6ac8a2217009866e451
Yomna <yomna@yomna-G41M-Combo.(none)>
2012-10-10 14:57:26 +0200

    adding span to titled plugin

--- billybeez/billybeez/settings.py
+++ billybeez/billybeez/settings.py
@@ -53,7 +53,7 @@ USE_TZ = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
-MEDIA_URL = '/media/    '
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -161,7 +161,7 @@ INSTALLED_APPS = (
     
     #our Custom plugins
     'cmsplugin_bootstrap_carousel',
-    'titledplugin',
+    'cmsplugin_titledplugin',
 )
 
 CMS_TEMPLATES = (
