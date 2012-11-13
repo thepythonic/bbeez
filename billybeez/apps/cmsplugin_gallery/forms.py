9900114949ca3a3ad11b1b1301423a0aed09a1d7
Yomna Fahmy <yomna.fahmy@scitecs.com>
2012-11-13 13:40:23 +0200

    deleting in-line gallery styles - not all

--- billybeez/apps/cmsplugin_gallery/forms.py
+++ billybeez/apps/cmsplugin_gallery/forms.py
@@ -9,7 +9,7 @@ class AdminImageWidget(AdminFileWidget):
         if value and getattr(value, "url", None):
             image_url = value.url
             file_name = str(value)
-            output.append(u' <a href="%s" target="_blank"><img src="%s" alt="%s" style="height: 100px;" /></a><br /> %s ' % \
+            output.append(u' <a href="%s" target="_blank"><img src="%s" alt="%s" class="gallery-img-height" /></a><br /> %s ' % \
                 (unicode(image_url), unicode(image_url), unicode(file_name), _('Change:')))
         output.append(super(AdminFileWidget, self).render(name, value, attrs))
         return mark_safe(u''.join(output))
